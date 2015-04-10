b1b92e5d7901ada6a67245ea20f01c4f8c89f613
Anuj Tomar <ankuto@gmail.com>
2015-04-09 20:48:45 -0700

    collections!

---
+++ collections.py
@@ -0,0 +1,15 @@
+#declaring lists
+array = []
+
+#length
+length = len(array)
+
+#adding to an array
+array.append("a pen")
+array.append("another pen")
+
+#index retrieval
+firstItem = array[0]
+secondItem = array[1]
+print (firstItem, secondItem)
+
