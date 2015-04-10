d5554f71042c5b955e91d4d738fa51e0fcc0d04f
Anuj Tomar <ankuto@gmail.com>
2015-04-09 20:37:12 -0700

    variables!

---
+++ variables.py
@@ -0,0 +1,26 @@
+#declaring integers
+somenumber = 3
+print (somenumber)
+
+#declaring longs
+bignumber = 203948203948203990293840293840988
+
+#declaring decimals
+anothernumber = 3.14
+
+#declaring strings
+somestring = "dam python u crzy"
+
+#declaring booleans
+boolean = True
+anotherbool = False
+null = None
+
+#finding variable type
+print(type(somenumber))
+print(type(anothernumber))
+print(type(bignumber))
+print(type(somestring))
+print(type(boolean))
+print(type(null))
+
